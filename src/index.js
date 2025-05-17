14975c1fc0a78ba9f07c41ebaf8bb6708974583a
Hanna Arkhipchuk <hanna_arkhipchuk@epam.com>
2025-05-17 13:16:49 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const AWS = require('aws-sdk');
+const sns = new AWS.SNS();
+
+exports.handler = async (event) => {
+    console.log('S3 Event:', JSON.stringify(event, null, 2));
+    
+    const record = event.Records[0].s3;
+    const bucketName = record.bucket.name;
+    const fileName = record.object.key;
+    
+    const message = {
+        default: `New file uploaded: ${fileName}`,
+        email: `File ${fileName} was uploaded to bucket ${bucketName}`,
+    };
+    
+    try {
+        await sns.publish({
+            TopicArn: process.env.SNS_TOPIC_ARN,
+            Message: JSON.stringify(message),
+            MessageStructure: 'json'
+        }).promise();
+        
+        console.log('Successfully sent SNS notification');
+        return { statusCode: 200, body: 'Notification sent' };
+    } catch (error) {
+        console.error('SNS publish error:', error);
+        throw error;
+    }
+};
